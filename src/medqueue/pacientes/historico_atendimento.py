173221ff9fdd978bcd9ddc6ce8dd684e2448857e
enzzo <enzzomauriciocosta4@gmail.com>
2026-09-04 12:07:06 -0300

    Atualiza e melhora o código

--- src/medqueue/pacientes/historico_atendimento.py
+++ src/medqueue/pacientes/historico_atendimento.py
@@ -1,11 +1,26 @@
+from paciente import pacientes_db
+
+historico = []
+
 def historico_atendimento():
-    acao = input("Informe a ação realizada: ")
-    id_paciente = input("Informe o ID do paciente: ")
+    id_paciente = input("informe o ID do paciente: ")
 
-    historico = []
-    registro = (acao, id_paciente)
+    if id_paciente in pacientes_db:
 
-    historico.append(registro)
+        paciente = pacientes_db[id_paciente]
 
-    print(f"Sua ação neste momento é: {acao}")
-    print(f"ID do paciente: {id_paciente}")
+        acao = input("Informe a ação realizada: ")
+
+        registro = (acao, paciente.id_paciente)
+
+        historico.append(registro)
+
+        print("\nHistórico registrado com sucesso!")
+        print(f"Paciente: {paciente.nome}")
+        print(f"Ação realizada: {acao}")
+        print(f"ID do paciente: {paciente.id_paciente}")
+
+    else:
+        print("Paciente não encontrado.")
+
+
