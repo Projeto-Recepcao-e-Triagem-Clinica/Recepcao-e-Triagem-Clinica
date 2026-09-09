201e58c8cb24c91716ae7a89fb1836cfc035d009
Gusta2331 <gustavosanmar2020@gmail.com>
2026-09-09 19:38:56 -0300

    atualização de codigo

--- src/medqueue/pacientes/paciente.py
+++ src/medqueue/pacientes/paciente.py
@@ -2,8 +2,10 @@ class Pacientes:
 
     proximo_id = 1
 
+    pacientes_db = {}
+    
     def __init__(self, nome, idade, numero_telefone, e_mail, especialidade):
-        self.id_paciente = f"{Pacientes.proximo_id:04d}"
+        self.id_paciente = Pacientes.proximo_id
         Pacientes.proximo_id += 1
 
         self.nome = nome
@@ -27,51 +29,3 @@ class Pacientes:
         print(f"E-mail: {self.e_mail}")
         print(f"Especialidade: {self.especialidade}")
         print(f"Status: {self.status}")
-
-pacientes_db = {}
-
-def cadastrar_paciente(nome, idade, numero_telefone, e_mail, especialidade):
-
-    paciente = Pacientes(
-        nome,
-        idade,
-        numero_telefone,
-        e_mail,
-        especialidade
-    )
-
-    pacientes_db[paciente.id_paciente] = paciente
-
-    print(f"Paciente {nome} cadastrado com sucesso!")
-    print(f"ID do paciente: {paciente.id_paciente}")
-
-    return paciente
-
-def buscar_paciente(id_paciente):
-
-    if id_paciente in pacientes_db:
-        return pacientes_db[id_paciente]
-
-    print("Paciente não encontrado.")
-    return None
-
-def remover_paciente(id_paciente):
-
-    if id_paciente in pacientes_db:
-        paciente = pacientes_db.pop(id_paciente)
-        print(f"Paciente {paciente.nome} removido com sucesso!")
-
-    else:
-        print("Paciente não encontrado.")
-
-def listar_pacientes():
-
-    if not pacientes_db:
-        print("Nenhum paciente cadastrado.")
-        return
-
-    print("\n===== PACIENTES CADASTRADOS =====")
-
-    for paciente in pacientes_db.values():
-        paciente.exibir_dados()
-        print("------------------------------")
