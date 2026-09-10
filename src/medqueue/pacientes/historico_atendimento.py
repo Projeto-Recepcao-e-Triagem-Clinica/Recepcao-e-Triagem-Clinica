d43b97de361e1c547b088ad5c7bf9b4c86c175e4
Gusta2331 <gustavosanmar2020@gmail.com>
2026-09-10 11:29:25 -0300

    correção codigo

--- src/medqueue/pacientes/historico_atendimento.py
+++ src/medqueue/pacientes/historico_atendimento.py
@@ -1,21 +1,12 @@
-def historico_atendimento():
-    id_paciente = input("informe o ID do paciente: ")
-
-    if id_paciente in pacientes_db:
-
-        paciente = pacientes_db[id_paciente]
-
-        acao = input("Informe a ação realizada: ")
-
 def registrar_acao(persistencia, acao, id_paciente):
-            registro = (acao, id_paciente)
-            persistencia.historico_atendimento.append(registro)
+    registro = (acao, id_paciente)
+    persistencia.historico_atendimento.append(registro)
 
 
 def desfazer_ultima_acao(persistencia):
     if not persistencia.historico_atendimento:
-            print("Não há ações para desfazer.")
-            return None
+        print("Não há ações para desfazer.")
+        return None
 
     return persistencia.historico_atendimento.pop()
 
